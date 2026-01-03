7e275640174f4be02795cedc8ef709440845dfe4
guy-cdn <70846389+guy-cdn@users.noreply.github.com>
2026-01-03 13:05:07 +0200

    Update

--- labs/lab10/induction/test1.tcl
+++ labs/lab10/induction/test1.tcl
@@ -1,14 +1,16 @@
+# Simple example of the base and induction step checks done by induction
+
 analyze -sv09 counter.v
 elaborate 
 clock clk
 reset rst
 
-# Base
+# Base (manual)
 #
 prove -property counter.ast -engine B -max_trace_length 1
 
-# Induction step
+# Induction step (manual)
 #
 #reset -none
 #property -name induction_step {c1 == c2 |=> c1 == c2}
-#prove -property induction_step -engine B -max_trace_length 2
+#prove -property induction_step -engine B -max_trace_length 2 -first_trace_attempt 2
