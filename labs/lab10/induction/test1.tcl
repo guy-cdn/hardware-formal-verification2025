6b0939bfd352e54512b470361462d068870ce45a
guy-cdn <70846389+guy-cdn@users.noreply.github.com>
2026-01-04 08:42:28 +0200

    Update test1.tcl

--- labs/lab10/induction/test1.tcl
+++ labs/lab10/induction/test1.tcl
@@ -12,5 +12,5 @@ prove -property counter.ast -engine B -max_trace_length 1
 # Induction step (manual)
 #
 #reset -none
-#property -name induction_step {c1 == c2 |=> c1 == c2}
+#assert -name induction_step {c1 == c2 |=> c1 == c2}
 #prove -property induction_step -engine B -max_trace_length 2 -first_trace_attempt 2
