72fd2d04391f1712e3897b8eca01e294861ea6b5
guy <guy@email>
2026-01-15 10:37:37 +0200

    Update

--- assignments/assignment04/coop.tcl
+++ assignments/assignment04/coop.tcl
@@ -4,9 +4,10 @@ elaborate -bbox_mul 1024 -top wrap
 clock clk
 reset rst
 
-# BEGIN 
-# Put the abstractions for the proof of <embedded>::wrap.valid_id before the next END
-#
-# END
+# Don't change anything above this line.
+
+# Put the abstractions for the proof of <embedded>::wrap.valid_id here.
+
+# Don't change anything below this line.
 
 check_return {prove -property {<embedded>::wrap.valid_id} -time_limit 300s -engine N -orch off} proven
